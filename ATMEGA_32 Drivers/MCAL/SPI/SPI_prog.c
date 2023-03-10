d96e55e162e54bb1e0473b4d9c7baed137f94262
Mohamed Ehab <80119396+Mohamed7899@users.noreply.github.com>
2023-03-11 00:46:46 +0200

    Add files via upload

---
+++ ATMEGA_32 Drivers/MCAL/SPI/SPI_prog.c
@@ -0,0 +1,148 @@
+﻿/*
+ * SPI_init.c
+ *
+ * Created: 04/03/2023 01:37:22 م
+ *  Author: NTI
+ */ 
+
+// Make MOSI, SCK, and SS pins directions as output.
+// Make MISO pin direction as input.
+// Make SS pin High.
+// Enable SPI in Master mode by setting SPE and MSTR bits in the SPCR register.
+// Set SPI Clock Rate Bits combination to define SCK frequency.
+#include "spi_master_config.h"
+#include "spi_init.h"
+
+
+void SPI_mstr_init(void)
+{
+	
+	
+	SET_BIT(sck_port_num,SCK);
+	SET_BIT( mosi_port_num,MOSI);
+	SET_BIT(ss_port_num,SS);
+	SET_BIT(SPCR,MSTR);
+	SET_BIT(SPCR,SPE);
+	DIO_PORTB_REG|=(1<<ss_port_num);
+	
+
+
+	#if data_order==MSB
+	CLEAR_BIT(SPCR,DORD);
+	
+	
+	#elif data_order==LSB
+	SET_BIT(SPCR, DORD);
+	#endif
+	
+	
+
+	#if sample==STD_LOW
+	CLEAR_BIT(SPCR,CPHA);
+	
+	#elif sample==STD_HIGH
+	SET_BIT(SPCR,CPHA);
+	#endif
+	
+	
+	#if clk_polarity==STD_LOW
+	CLEAR_BIT(SPCR,CPOL);
+	#elif clk_polarity==STD_HIGH
+	SET_BIT(SPCR,CPOL);
+	#endif
+	
+	#if clock_rate == Foscprescaler4 
+	CLEAR_BIT(SPCR,SPR0);
+	CLEAR_BIT(SPCR,SPR1);
+	CLEAR_BIT(SPCR,SPI2X);
+	
+	#elif clock_rate ==Foscprescaler16
+	SET_BIT(SPCR,SPR0);
+	CLEAR_BIT(SPCR,SPR1);
+	CLEAR_BIT(SPCR,SPI2X);
+	
+	#elif clock_rate ==Foscprescaler64
+	CLEAR_BIT(SPCR,SPR0);
+	SET_BIT(SPCR,SPR1);
+	CLEAR_BIT(SPCR,SPI2X);
+		
+	#elif clock_rate ==Foscprescaler128
+	SET_BIT(SPCR,SPR0);
+	SET_BIT(SPCR,SPR1);
+	CLEAR_BIT(SPCR,SPI2X);
+
+	#elif clock_rate ==Foscprescaler2
+	CLEAR_BIT(SPCR,SPR0);
+	CLEAR_BIT(SPCR,SPR1);
+	SET_BIT(SPCR,SPI2X);	
+	
+	#elif clock_rate ==Foscprescaler8
+	SET_BIT(SPCR,SPR0);
+	CLEAR_BIT(SPCR,SPR1);
+	SET_BIT(SPCR,SPI2X);	
+	
+	#elif clock_rate ==Foscprescaler32
+	CLEAR_BIT(SPCR,SPR0);
+	SET_BIT(SPCR,SPR1);
+	SET_BIT(SPCR,SPI2X);
+	#endif
+	SET_BIT(DIO_PORTB_REG,SS);
+}
+
+void SPI_slave_init(void){
+	
+	CLEAR_BIT(sck_port_num,SCK);
+	SET_BIT( miso_port_num,MISO);
+	CLEAR_BIT( ss_port_num,SS);	
+	CLEAR_BIT( mosi_port_num,MOSI);
+
+	#if sample==STD_LOW
+	CLEAR_BIT(SPCR,CPHA);
+	
+	#elif sample==STD_HIGH
+	SET_BIT(SPCR,CPHA);
+	#endif
+	
+	
+	#if clk_polarity==STD_LOW
+	CLEAR_BIT(SPCR,CPOL);
+	#elif clk_polarity==STD_HIGH
+	SET_BIT(SPCR,CPOL);
+	#endif
+	
+	SET_BIT(SPCR,SPE);
+}
+
+u8 SPI_mstr_transev_byte(u8 byte){
+	u8 buffer;
+	SPDR=byte;
+	while (!(CHECK_BIT(SPDR,SPIF)));
+	buffer=SPDR;
+	return buffer;
+}
+
+void SPI_mstr_send_byte(u8 byte){
+	CLEAR_BIT(DIO_PORTB_REG,SS);
+	SPDR=byte;
+	while (!(SPDR & (1<<SPIF)));
+	
+}
+
+
+
+void SPI_slave_transmit_byte(u8 byte){
+	while(!(SPDR & (1<<SPIF)));
+	SPDR=byte;
+}
+
+u8 SPI_slave_recieve_byte(void){
+	while(!(SPDR & (1<<SPIF)));
+	return SPDR;
+}
+
+u8 SPI_master_recieve_byte(void){
+	u8 buffer=0xff;
+	while(!(SPDR & (1<<SPIF)));
+	SPI_mstr_send_byte(buffer);
+	return SPDR;
+}
---
+++ ATMEGA_32 Drivers/MCAL/SPI/spi_master_config.h
@@ -0,0 +1,36 @@
+﻿
+#ifndef SPI_CONFIG_H_
+#define SPI_CONFIG_H_
+
+#include "std_types.h"
+#include "spi_private_reg.h"
+#include "dio_reg.h"
+
+#define  mosi_port_num DIO_DDRB_REG
+
+#define miso_port_num DIO_DDRB_REG
+
+#define  ss_port_num DIO_DDRB_REG
+
+#define  sck_port_num DIO_DDRB_REG
+
+#define clock_rate Foscprescaler4
+
+#define data_order MSB
+
+#define sample STD_LOW
+
+#define clk_polarity STD_HIGH
+
+#define MOSI	5
+
+#define MISO	6
+
+#define SCK		7
+
+#define SS	    4
+
+#define double_speed 0
+
+
+#endif /* SPI_CONFIG_H_ */
